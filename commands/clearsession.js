82fbfc76e80ddbede51615f9c94c8f970dc02e47
ghost-xmd <ghostviewer99@gmail.com>
2025-11-10 09:30:54 +0300

    Update clearsession.js

--- commands/clearsession.js
+++ commands/clearsession.js
@@ -7,8 +7,8 @@ const channelInfo = {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+             newsletterJid: '120363401087525436@newsletter',
+                    newsletterName: 'HACKLINK',
             serverMessageId: -1
         }
     }
