fadeed33b12f21685d7095a46a9529e7ffd6ef76
ghost-xmd <ghostviewer99@gmail.com>
2025-11-10 09:27:09 +0300

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -1,7 +1,9 @@
 const settings = require("../settings");
+const axios = require("axios");
+
 async function aliveCommand(sock, chatId, message) {
     try {
-        const message1 = `*🤖 Knight Bot is Active!*\n\n` +
+        const message1 = `*🤖 *GHOST-𝗫𝗠𝗗* is Active!*\n\n` +
                        `*Version:* ${settings.version}\n` +
                        `*Status:* Online\n` +
                        `*Mode:* Public\n\n` +
@@ -18,11 +20,36 @@ async function aliveCommand(sock, chatId, message) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '120363401087525436@newsletter',
+                    newsletterName: 'HACKLINK',
                     serverMessageId: -1
                 }
             }
+        }, { quoted: message });
+
+        const audioUrls = [
+    'https://files.catbox.moe/hpwsi2.mp3',
+    'https://files.catbox.moe/xci982.mp3',
+    'https://files.catbox.moe/utbujd.mp3',
+    'https://files.catbox.moe/w2j17k.m4a',
+    'https://files.catbox.moe/851skv.m4a',
+    'https://files.catbox.moe/qnhtbu.m4a',
+    'https://files.catbox.moe/lb0x7w.mp3',
+    'https://files.catbox.moe/efmcxm.mp3',
+    'https://files.catbox.moe/gco5bq.mp3',
+    'https://files.catbox.moe/26oeeh.mp3',
+    'https://files.catbox.moe/a1sh4u.mp3',
+    'https://files.catbox.moe/vuuvwn.m4a',
+    'https://files.catbox.moe/wx8q6h.mp3',
+    'https://files.catbox.moe/uj8fps.m4a' // replace with your audio url 1
+            
+        ];
+
+        const randomAudioUrl = audioUrls[Math.floor(Math.random() * audioUrls.length)];
+
+        await sock.sendMessage(chatId, {
+            audio: { url: randomAudioUrl },
+            mimetype: 'audio/mp4'
         }, { quoted: message });
     } catch (error) {
         console.error('Error in alive command:', error);
